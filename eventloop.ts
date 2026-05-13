8c670563735e25f361d241cc421ea8ff78e7df6e
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2026-05-13 15:15:57 +0200

    represent behaviour as JSON

--- eventloop.ts
+++ eventloop.ts
@@ -13,20 +13,60 @@ const actionPriorityQueue = new Heap(customPriorityComparator);
 
 const broker = new Broker(actionPriorityQueue);
 
-
-const knightRules = `root {
-        sequence {
-            action [Guard]
-        }
-    }`;
+const knightRules = `
+{
+  "type": "root",
+  "child": {
+    "type": "sequence",
+    "children": [
+      {
+        "type": "action",
+        "call": "Guard"
+      }
+    ]
+  }
+}
+`
 const knight = new KnightActor(knightRules, broker, actionPriorityQueue, "knt01a");
-const healerRules = `root {
-        sequence {
-            action [Heal]
-        }
-    }`;
+const healerRules = `
+{
+  "type": "root",
+  "child": {
+    "type": "sequence",
+    "children": [
+      {
+        "type": "action",
+        "call": "Heal"
+      }
+    ]
+  }
+}
+`
 const healer = new HealerActor(healerRules, broker, actionPriorityQueue,"hel01a");
 
 const fighter = new FighterActor(healerRules, broker, actionPriorityQueue, "fgt03b");
 
 fighter.enemyAttack();
+
+/**
+ * {
+ *     "type": "root",
+ *     "child": {
+ *         "type": "sequence",
+ *         "children": [
+ *             {
+ *                 "type": "action",
+ *                 "call": "Walk"
+ *             },
+ *             {
+ *                 "type": "action",
+ *                 "call": "Fall"
+ *             },
+ *             {
+ *                 "type": "action",
+ *                 "call": "Laugh"
+ *             }
+ *         ]
+ *     }
+ * }
+ */
